e9d9609044a67f828dc6661c1a4219817fe1f01f
PedroHenmalaquias <pedromalaquias2223@gmail.com>
2024-02-02 15:01:02 -0300

    as

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 
 let count = 1;
 document.getElementById("radio1").checked = true;
-setInterval(function(){
-nextImage()
-},5000)
-
+setInterval(function () {
+    nextImage()
+}, 5000);
 function nextImage() {
     count++;
-    if(count > 2) count = 1;
-    document.getElementById("radio"+count).checked = true;
-}
-
-
+    if (count > 2) count = 1;
+    document.getElementById("radio" + count).checked = true;
+};
 const elements = document.querySelectorAll(".hidden");
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting) entry.target.classList.add("show");
+        if (entry.isIntersecting) entry.target.classList.add("show");
         else entry.target.classList.remove("show");
     })
 });
-elements.forEach((element)  => observer.observe(element));
+elements.forEach((element) => observer.observe(element));
 const popUp = document.querySelector("dialog");
 const openButton = document.querySelector("#Link-orçamento");
 const closeButton = popUp.querySelector("#botao-cancelar");
-openButton.onclick = function(){
-    popUp.showModal();
-}
-closeButton.onclick = function(){
+openButton.onclick = function () {
+    popUp.showModal()
+};
+closeButton.onclick = function () {
     popUp.close();
 };
 function openWhatsapp() {
@@ -35,13 +32,15 @@ function openWhatsapp() {
     const email = document.getElementById("input-email").value;
     const cidadeEstado = document.getElementById("input-cidade").value;
     const valorConta = document.getElementById("input-valorConta").value;
-    const url = "https://wa.me/5547991184632?text=" // Seu numero
-      + "*Formulário de Contato*" + "%0a" // Mensagem personalizada
-      + "%0a" // Quebra de linha
-      + "*Nome*: " + nome + "%0a" // Dados do formulário
-      + "*Telefone*: " + telefone + "%0a"
-      + "*E-mail*: " + email + "%0a"
-      + "*Cidade / Estado*: " + cidadeEstado + "%0a%0"
-      + "*Valor da conta*: R$ " + valorConta;
+    const Mensagem = document.getElementsByClassName("input-mensagem")[1];
+    const url = "https://wa.me/5561994180249?text=" // Seu numero
+        + "*Formulário de Contato*" + "%0a" // Mensagem personalizada
+        + "%0a" // Quebra de linha
+        + "*Nome*: " + nome + "%0a" // Dados do formulário
+        + "*Telefone*: " + telefone + "%0a"
+        + "*E-mail*: " + email + "%0a"
+        + "*Cidade / Estado*: " + cidadeEstado + "%0a%0"
+        + "*Valor da conta*: R$ " + valorConta; "%0a%0"
+        + "Mensagem: " +Mensagem;
     window.open(url, '_blank').focus();
-  }
+};
